96606169ddd48499a7b32ae6c4e621831b6c516b
Farid Izzat <i.faridizzat@gmail.com>
2024-06-04 23:31:47 +0800

    add: validation and hash

--- controller/user/createNewUser.js
+++ controller/user/createNewUser.js
@@ -1,4 +1,5 @@
 import { pool } from "../../database/index.js";
+import bcrypt from "bcrypt";
 
 const query = `
 INSERT INTO T_USER (name, email, password) VALUES ($1, $2, $3)
@@ -10,10 +11,28 @@ export const createNewUser = async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    console.log(name, email, password);
+    //   validate the request body is not empty
+    if (!name || !password || !email) {
+      return res.status(400).json({
+        message: "name, password, email is required",
+      });
+    }
 
-    const dbRes = await pool.query(query, [name, email, password]);
-    console.log(dbRes);
+    //   validate email string using regex
+    const emailRegex = /\S+@\S+\.\S+/;
+    const isValidEmail = emailRegex.test(email);
+    if (!isValidEmail) {
+      return res.status(400).json({
+        message: "Invalid email",
+      });
+    }
+
+    //hash the password
+    const saltRounds = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+    //   insert new user into database
+    const dbRes = await pool.query(query, [name, email, hashedPassword]);
 
     res.status(201).json({
       message: "User created successfully",
