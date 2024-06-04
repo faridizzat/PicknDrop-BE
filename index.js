7f42a1688853f76c2545067d416e85919297a2c6
Farid Izzat <i.faridizzat@gmail.com>
2024-06-04 23:24:31 +0800

    fix: create user

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import { healthCheck, healthCheckJson } from "./controller/health.js";
 import { databaseInit } from "./database/index.js";
+import { createNewUser } from "./controller/user/createNewUser.js";
 
 const app = express();
 const PORT = 3000;
 
-app.get("/helloworld", healthCheck);
-app.get("/helloworld-json", healthCheckJson);
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //db initialization
 databaseInit();
+
+//health check
+app.get("/helloworld", healthCheck);
+app.get("/helloworld-json", healthCheckJson);
+
+//register new user
+app.post("/register", createNewUser);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on http://localhost:${PORT}`);
