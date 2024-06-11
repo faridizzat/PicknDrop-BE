e1a806e6bcc15dd6046e3718f8b9a20f83de67f4
Farid Izzat <i.faridizzat@gmail.com>
2024-06-12 00:24:21 +0800

    remove noise

--- controller/user/getUserById.js
+++ controller/user/getUserById.js
@@ -4,7 +4,6 @@ const query = `SELECT id, name, email FROM T_USER where id=$1;`;
 const getUserById = async (req, res) => {
   try {
     const userId = req.userId;
-    console.log(userId);
     const dbRes = await pool.query(query, [userId]);
     const data = dbRes.rows[0];
     res.status(200).json({
